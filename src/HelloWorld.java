66d5e8c9e0cc528a0ae9cc910614af871ea54718
chen'yuan'yuan <chenyuanyuan@sunline.cn>
2020-09-15 20:06:32 +0800

    update 0915

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,11 @@
+import java.time.Clock;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello");
+        String phoneTokenUrl = "https://openapi.dustess.com/oauth/token/getToken";
+        long millis = Clock.systemDefaultZone().millis();
+
+        String url = phoneTokenUrl + "?timestamp=" + millis;
+        System.out.println(url);
     }
 }
